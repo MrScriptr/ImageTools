024e239d3605bb45537a974be458ca571faebb2a
MrScriptr <codermohammad110@gmail.com>
2025-12-27 04:09:04 +0000

    added command parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{clone, env};
+
+enum Command {
+    Convert(String),
+}
+
+enum Error {
+    InvalidCommand
+}
+
+fn parseargs(args: Vec<String>) -> Result<Command, Error>  {
+    let path = args[1].clone();
+    let command = args[2].clone().to_lowercase();
+
+    match command.as_str() {
+        "convert" => Ok(Command::Convert(path)),
+        _ => Err(Error::InvalidCommand)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
 }
